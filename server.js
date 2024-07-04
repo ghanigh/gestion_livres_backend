38d5014c0223b5ebf2c0bc02cd65c9aba5da1bd9
ghanigh <rhani.94@proton.me>
2024-07-04 16:04:03 +0200

    Jenkinsfile

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use('/api', routes);
 
 sequelize.sync().then(() => {
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000');
+    app.listen(8080, () => {
+        console.log('Server is running on port 8080');
     });
 });
